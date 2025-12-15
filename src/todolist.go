16c9c898f5e1b45cb366e1f2d5bc105e546e3918
Danniel Hansel <hansel.danniel@gmail.com>
2025-12-14 22:44:17 -0600

    connect to mysql db

--- src/todolist.go
+++ src/todolist.go
@@ -6,7 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var db, _ := gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
@@ -20,6 +25,7 @@ func init() {
 }
 
 func main() {
+	defer db.Close()
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
