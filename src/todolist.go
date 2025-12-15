866e4c726bf07d0add11d7edd50cccfebaa46807
Danniel Hansel <hansel.danniel@gmail.com>
2025-12-14 22:52:17 -0600

    auto migrate todoItem ORM model

--- src/todolist.go
+++ src/todolist.go
@@ -13,6 +13,12 @@ import (
 
 var db, _ := gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
+type TodoItemModel struct {
+	Id int `gorm:"primary_key"`
+	Description string
+	Completed bool
+}
+
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
@@ -26,6 +32,10 @@ func init() {
 
 func main() {
 	defer db.Close()
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
+	
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
